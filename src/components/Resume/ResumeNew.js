a80cd39a9bda63822015816f122f860ca1aa59aa
mariomarquesto <99926074+mariomarquesto@users.noreply.github.com>
2025-07-20 22:05:50 -0300

    Actualizando el cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import firstPdf from "./MARQUESTO-MARIO-ANIBAL-FRANCISCO.pdf";
+import firstPdf from "./MARQUESTO-MARIO-ANIBAL-FRANCISCO-ESP.pdf";
 import secondPdf from "./MARQUESTO-MARIO-ANIBAL-FRANCISCO-IN.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
